3ab75bd7a8e8b5932523e387e39db91121f47b17
fredluggard <fredrick.ebuka33@gmail.com>
2025-02-06 19:37:34 +0100

    fix:: updated for production

--- components/comments/index.tsx
+++ components/comments/index.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import { Stack, Title } from "@mantine/core";
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
 const Comments = ({ postId }: { postId: string }) => {
   //   const [comments, setComments] = useState<any>(null);
   const url = process.env.NEXT_PUBLIC_BASE_URL;
 
-  useEffect(() => {
-    const fetchBlog = async () => {
-      try {
-        const response = await fetch(`${url}/api/v1/posts/${postId}`);
-        const data = await response.json();
-        // setComments(data.data || null);
-      } catch (error) {
-        console.error("Error fetching recent blog:", error);
-      }
-    };
+  //   useEffect(() => {
+  //     const fetchBlog = async () => {
+  //       try {
+  //         const response = await fetch(`${url}/api/v1/posts/${postId}`);
+  //         const data = await response.json();
+  //         // setComments(data.data || null);
+  //       } catch (error) {
+  //         console.error("Error fetching recent blog:", error);
+  //       }
+  //     };
 
-    fetchBlog();
-  });
+  //     fetchBlog();
+  //   });
 
   return (
     <Stack>
