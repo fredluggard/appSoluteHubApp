fe2c5a07da12d66a7170e0f04a09743cbe6a1562
fredluggard <fredrick.ebuka33@gmail.com>
2025-02-06 19:35:24 +0100

    fix:: fixed the comments component for production

--- components/comments/index.tsx
+++ components/comments/index.tsx
@@ -4,7 +4,7 @@ import { Stack, Title } from "@mantine/core";
 import React, { useEffect, useState } from "react";
 
 const Comments = ({ postId }: { postId: string }) => {
-  const [comments, setComments] = useState<any>(null);
+  //   const [comments, setComments] = useState<any>(null);
   const url = process.env.NEXT_PUBLIC_BASE_URL;
 
   useEffect(() => {
@@ -12,14 +12,14 @@ const Comments = ({ postId }: { postId: string }) => {
       try {
         const response = await fetch(`${url}/api/v1/posts/${postId}`);
         const data = await response.json();
-        setComments(data.data || null);
+        // setComments(data.data || null);
       } catch (error) {
         console.error("Error fetching recent blog:", error);
       }
     };
 
     fetchBlog();
-  }, []);
+  });
 
   return (
     <Stack>
