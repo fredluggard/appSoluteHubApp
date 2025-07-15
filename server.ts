098de6a6140a322f781e97b0412742b48d80edee
Fredrick Aniebonam <fredrick.ebuka33@gmail.com>
2025-07-15 18:19:18 +0100

    updated the server

--- server.ts
+++ server.ts
@@ -3,19 +3,16 @@ import { parse } from "url";
 import next from "next";
 
 const port = parseInt(process.env.PORT || "3000", 10);
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url!, true);
     handle(req, res, parsedUrl);
-  }).listen(port);
-
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? "development" : process.env.NODE_ENV
-    }`
-  );
+  }).listen(port, () => {
+    console.log(
+      `✅ Server listening at http://localhost:${port} in PRODUCTION mode`
+    );
+  });
 });
